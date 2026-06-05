3d4144e00c0244e39c1f778e88ffa2744cac0f56
KimHeng2111 <lengkimheng2111@gmail.com>
2026-06-05 22:16:57 +0700

    test RateLimit for each client

--- account/src/main/java/com/kimheng/account/controller/AccountController.java
+++ account/src/main/java/com/kimheng/account/controller/AccountController.java
@@ -1,7 +1,10 @@
 package com.kimheng.account.controller;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kimheng.account.dto.AccountDTO;
 import com.kimheng.account.service.AccountService;
 
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,22 +28,36 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/account")
 public class AccountController {
 	private final AccountService service;
+	private final RateLimiterRegistry rateLimiterRegistry;
+
 	@PostMapping
-	public ResponseEntity<AccountDTO> createAccount(@RequestBody AccountDTO dto){
+	public ResponseEntity<AccountDTO> createAccount(@RequestBody AccountDTO dto) {
 		AccountDTO account = service.createAccount(dto);
 		return ResponseEntity.ok(account);
 	}
+
 	@GetMapping("{customer_id}")
-	public ResponseEntity<List<AccountDTO>> findAccountByCustomerId(@PathVariable("customer_id") String cumstomerId){
+	public ResponseEntity<List<AccountDTO>> findAccountByCustomerId(@PathVariable("customer_id") String cumstomerId) {
 		List<AccountDTO> byCustomerId = service.findByCustomerId(cumstomerId);
 		return ResponseEntity.ok(byCustomerId);
 	}
+
 	@GetMapping
-	public ResponseEntity<List<AccountDTO>> findAllAccount(){
-		
-		return ResponseEntity.ok(service.findAllAccount());
+	public ResponseEntity<List<AccountDTO>> findAllAccount(HttpServletRequest request) {
+		String userId = request.getRemoteAddr();
+		RateLimiter rateLimiter = rateLimiterRegistry.rateLimiter(userId, () -> RateLimiterConfig.custom()
+				.limitForPeriod(2).limitRefreshPeriod(Duration.ofSeconds(20)).timeoutDuration(Duration.ZERO).build());
+		List<AccountDTO> accounts = new ArrayList<AccountDTO>();
+		try {
+			accounts = RateLimiter.decorateSupplier(rateLimiter, () -> service.findAllAccount()).get();
+			System.out.println("==============GetAccountListSuccess================");
+		} catch (RequestNotPermitted e) {
+			System.out.println("==============GetAccountListFailed================");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		}
+		return ResponseEntity.ok(accounts);
 	}
-	//Test RateLimiter
+	// Test RateLimiter
 //	@RateLimiter(name = "testRateLimiter" , fallbackMethod = "sayHi")
 //	@GetMapping("sayHellow")
 //	public String sayHello() {
